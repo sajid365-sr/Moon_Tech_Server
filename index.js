13d2e77a411ed46672d90919b8b539da36eaed0f
sajid365-sr <sajidsorker86@gmail.com>
2023-03-15 08:46:06 +0600

    add and delete product route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +10,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 
@@ -28,10 +27,26 @@ const run = async() =>{
             const query = {};
             const result = await productsCollection.find(query).toArray();
 
+
             res.send(result);
         })
 
+        // Add products
+        app.post('/product', async (req, res) => {
+            const product = req.body;
 
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // Delete products
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const result = await productsCollection.deleteOne({_id: ObjectId(id)});
+
+            res.send(result);
+        })
 
     }finally{
 
